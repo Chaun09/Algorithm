4a14cb9e31bad990a1d7b35160d0a771baccf4bc
chouming <bangcac24@gmail.com>
2024-07-04 11:29:14 +0700

    java

--- Linear Search/linearsearch.js
+++ Linear Search/linearsearch.js
@@ -1,31 +1,26 @@
 function LinearSearch(arr) {
   n = arr.length;
   var x = 9;
-  let rep = "Ko tìm thấy số phù hợp";
+  let rep = "Mảng chứa kí tự đặc biệt";
   let r = "Mảng rỗng";
+
   if (n == 0) {
     console.log(r);
   } else {
-    if (n == 1) {
+    if (!Number.isInteger(arr)) {
+      console.log(rep);
+    } else {
       for (let i = 0; i < n; i++) {
-        if (x == arr[0]) {
-          console.log("Số" + x + "ở vị trí :0");
-        } else {
-          console.log(rep + arr);
+        // console.log(arr[i]);
+        if (x == arr[i]) {
+          console.log("Số " + x + " ở vị trí:" + i);
+          // return `Số ${x} ở vị trí :${i}`;
         }
       }
     }
-    for (let i = 0; i < n; i++) {
-      // console.log(arr[i]);
-      if (x == arr[i]) {
-        console.log("Số " + x + " ở vị trí:" + i);
-        // return `Số ${x} ở vị trí :${i}`;
-      }
-    }
   }
-  console.log(rep);
 }
 
-const array = [8, 10];
+const array = [9, "string"];
 
 LinearSearch(array);
---
+++ javaeledevo/Main.java
@@ -0,0 +1,39 @@
+package javaeledevo;
+// folder chứa các class
+// import java.util.Arrays;//thêm thư viện 
+// import java.util.Iterator;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+//		public : phạm vi truy cập
+//		Static: phương thức tĩnh
+//		Void: 0 trả về tham số 
+//		main: tên hàm
+//		String[] args: tham số truyền vào
+		// int[] arrr= new int[5];
+		// int a = 3;
+		// System.out.println(a);
+		// double b = 3.1415;
+		// float k =  3.124124124;
+		// System.out.println(b);
+		// boolean c = true;
+		// char d = 'r';
+		// System.out.println(d);
+		// String s = "thang beo";
+		// System.out.println(s.length());
+		// int e[] = {1, 2, 3};
+		// System.out.println(Arrays.toString(e));
+		// for (int i = 0; i < e.length; i++) {
+
+		// 	if(e[i] > 2)
+		// 		System.out.print(e[i] );
+		// }
+		
+		 ConNguoi conNguoi = new ConNguoi(1.5, "Thang", 120);
+		 System.out.println(conNguoi);
+		// String name = "Nguyễn Minh Châu";
+		// System.out.println(name);
+	}
+}
