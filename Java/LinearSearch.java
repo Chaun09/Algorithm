c6db4e3e523da1f9301d613ea1b1c0478d3db63a
chouming <bangcac24@gmail.com>
2024-07-09 11:36:58 +0700

    push1

--- Java/LinearSearch.java
+++ Java/LinearSearch.java
@@ -6,8 +6,22 @@ import java.util.Arrays;
 public class LinearSearch {
 
     public static void main(String[] args) {
+
+        // int a[] = { 1, 2, 3 };
+        // int n = a.length;
+        // Scanner scanner = new Scanner(System.in);
+
+        // for (int i = 0; i < n; i++) {
+        // System.out.print("Nhap so:");
+        // int x = scanner.nextInt();
+        // if (x == a[n - 1]) {
+        // System.out.print("Phan tu da nhap la phan tu cuoi");
+
+        // }
+
+        // }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap tong so phan tu  trong day:");
+        System.out.println("Nhap tong so phan tu trong day:");
         int n = scanner.nextInt();
         if (n == 0) {
             Ketqua kq = new Ketqua(false, "Day ko co phan tu");
@@ -28,15 +42,17 @@ public class LinearSearch {
                     Ketqua kq = new Ketqua(true, "So can tim o vi tri " + i);
                     System.out.println(kq.Export());
 
+                } else {
+                    Ketqua kq = new Ketqua(false, "Khong phai so can tim");
+                    System.out.println(kq.Export());
                 }
 
             }
-            System.out.println(Arrays.toString(arr));
-            Ketqua kq = new Ketqua(false, "Khong co so can tim");
-            System.out.println(kq.Export());
 
-            // System.out.println("Khong co so can tim");
+            System.out.println(Arrays.toString(arr));
+
         }
+
     }
 
 }
